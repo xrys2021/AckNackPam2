08a836684d2b8cdbdf2deea861becd90e55905bf
Nathanhunter11 <nathanhunter11@gmail.com>
2021-07-16 11:38:31 -0700

    Dialogue assets

---
+++ Assets/Nathan's Dialogue System/DialogueBaseClass.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{
+ public class DialogueBaseClass : MonoBehaviour
+ {
+    protected IEnumerator WriteText(string input, Text textHolder, Color textColor, Font textFont, float delay, AudioClip sound)
+    {
+        textHolder.color = textColor;
+        textHolder.font = textFont;
+        
+        for(int i=0; i<input.Length; i++)
+        {
+            textHolder.text += input[i];
+            //play letter sound
+            yield return new WaitForSeconds(delay);
+        }
+
+    }
+ }
+}
+
