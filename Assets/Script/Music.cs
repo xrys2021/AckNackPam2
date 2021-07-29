05243c59d9ee6cb2f2f292676145491915087e8d
Nathanhunter11 <nathanhunter11@gmail.com>
2021-07-29 10:47:33 -0700

    Sound Button

---
+++ Assets/Script/Music.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Music : MonoBehaviour
+{
+    static Music instance = null;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+        instance = this;
+        GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+    public void ToggleSound()
+    {
+        if(PlayerPrefs.GetInt("Muted", 0) == 0) {
+            PlayerPrefs.SetInt("Muted", 1);
+            //AudioListener.volume = 1;
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Muted", 0);
+            //AudioListener.volume = 0;
+        }
+    }
+
+}   
